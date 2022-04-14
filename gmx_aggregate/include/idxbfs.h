25192936e13cc9c3fcf3727d552fd9e6118a8fc8
jpablo <jpablo.galvez@um.es>
2022-04-14 16:48:40 +0200

    FILE ADDED

---
+++ gmx_aggregate/include/idxbfs.h
@@ -0,0 +1,8 @@
+       integer  ::  inode   !  Node index
+       integer  ::  jnode   !  Node index
+       integer  ::  knode   !  Node index
+       integer  ::  iqueue  !  Queue index
+       integer  ::  nqueue  !  Number of queue elements
+       integer  ::  nnmol   !  Number of aggregates index
+       integer  ::  ntag    !  Size of the aggregate
+       integer  ::  nntag   !  Size of the aggregate index
